18e83776c45a759f0719d32e339913530df2b1d2
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2024-01-05 23:00:19 -0500

    WIP

--- MazeRunner/MazeRunner.Console/MazeRunnerGame.cs
+++ MazeRunner/MazeRunner.Console/MazeRunnerGame.cs
@@ -1,4 +1,5 @@
 ﻿using MazeRunner.Models.Enunms;
+using MazeRunner.Models.Models;
 using MazeRunner.Models.Requests;
 using MazeRunner.Models.Responses;
 using MazeRunner.Services;
@@ -8,21 +9,24 @@ namespace MazeRunner.Console
     public class MazeRunnerGame
     {
         private readonly IApiService _apiService;
+        private readonly int _mazeSize;
         private Guid _mazeUid;
         private Guid _gameUid;
         private bool _completed;
+        private Operations _direction;
 
         public MazeRunnerGame(IApiService apiService)
         {
             _apiService = apiService;
+            _mazeSize = 10;
         }
 
         public async Task<string> CreateMazeAsync()
         {
             var request = new CreateNewMazeRequest
             {
-                Height = 25,
-                Width = 25
+                Height = _mazeSize,
+                Width = _mazeSize
             };
             var response = await _apiService.PostAsync<CreateNewMazeRequest, CreateNewMazeResponse>("/api", "/Maze", request);
             if (!response.WasSuccess)
@@ -53,9 +57,11 @@ namespace MazeRunner.Console
         public async Task<string> RunGameAsync()
         {
             var movement = Operations.GoEast;
-            var movements = "Movements: ";
+            _direction = Operations.GoEast;
+            var movements = "Movements:\n";
             var movementsCount = 0;
-            while (!_completed || movementsCount > 625)
+            var maxMovements = _mazeSize * _mazeSize * 2;
+            while (!_completed && movementsCount < maxMovements)
             {
                 var request = new OperationRequest
                 {
@@ -65,32 +71,22 @@ namespace MazeRunner.Console
                 var response = await _apiService.PostAsync<OperationRequest, TakeALookResponse>("/api", $"/Game/{_mazeUid}/{_gameUid}/", request);
                 if (!response.WasSuccess)
                 {
-                    return $"{movements}\nError trying the next movement.";
+                    if (_direction == Operations.GoEast) _direction = Operations.GoSouth;
+                    else if (_direction == Operations.GoSouth) _direction = Operations.GoWest;
+                    else if (_direction == Operations.GoWest) _direction = Operations.GoNorth;
+                    else return $"{movements}\nError trying the next movement.";
+                    continue;
                 }
 
                 movements += $"{movement} ({response.Result!.MazeBlockView.CoordX}, {response.Result!.MazeBlockView.CoordY})\n ";
+                var lastMovement = movements.Length > 80 ? movements.Substring(movements.Length - 80, 80) : movements;
                 _completed = response.Result!.Game.Completed;
                 if (_completed)
                 {
                     break;
                 }
 
-                if (!response.Result.MazeBlockView.SouthBlocked)
-                {
-                    movement = Operations.GoSouth;
-                }
-                else if (!response.Result.MazeBlockView.EastBlocked)
-                {
-                    movement = Operations.GoEast;
-                }
-                else if (!response.Result.MazeBlockView.WestBlocked)
-                {
-                    movement = Operations.GoWest;
-                }
-                else
-                {
-                    movement = Operations.GoNorth;
-                }
+                movement = GetNextMovement(response.Result.MazeBlockView);
             }
 
             if (_completed)
@@ -98,7 +94,44 @@ namespace MazeRunner.Console
                 return $"{movements}\nGame over.";
             }
 
-            return "The maze has no solution\nGame over.";
+            return $"{movements}\nThe maze has no solution\nGame over.";
+        }
+
+        private Operations GetNextMovement(Mazeblockview mazeBlockView, Operations _direction = Operations.GoEast)
+        {
+            switch (_direction)
+            {
+                case Operations.GoEast:
+                    if (!mazeBlockView.EastBlocked) return Operations.GoEast;
+                    if (!mazeBlockView.SouthBlocked) return Operations.GoSouth;
+                    _direction = Operations.GoWest;
+                    return _direction;
+
+                case Operations.GoWest:
+                    if (!mazeBlockView.SouthBlocked)
+                    {
+                        _direction = Operations.GoSouth;
+                        return _direction;
+                    }
+                    if (!mazeBlockView.WestBlocked) return Operations.GoWest;
+                    _direction = Operations.GoNorth;
+                    return _direction;
+
+                case Operations.GoSouth:
+                    if (!mazeBlockView.SouthBlocked) return Operations.GoSouth;
+                    if (!mazeBlockView.EastBlocked) return Operations.GoEast;
+                    _direction = Operations.GoNorth;
+                    return _direction;
+
+                case Operations.GoNorth:
+                    if (!mazeBlockView.EastBlocked)
+                    {
+                        _direction = Operations.GoEast;
+                        return _direction;
+                    }
+                    return Operations.GoNorth;
+            }
+            return Operations.GoEast;
         }
     }
 }
